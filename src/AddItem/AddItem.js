d3af8b88c60e8597a26aea8542d54f916e619962
Spencer Davis <spencerdavis2012@gmail.com>
2019-04-04 22:08:21 -0500

    first commit, lets build this thing!

---
+++ src/AddItem/AddItem.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class AddItem extends Component {
+  render() {
+    return (
+      <div className="AddItem">
+        <h1>Add Item</h1>
+      </div>
+    );
+  }
+}
+
+export default AddItem;
---
+++ src/ItemsDisplay/ItemsDisplay.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class ItemsDisplay extends Component {
+  render() {
+    return (
+      <div className="ItemsDisplay">
+        <h1>Items Display</h1>
+      </div>
+    );
+  }
+}
+
+export default ItemsDisplay;
